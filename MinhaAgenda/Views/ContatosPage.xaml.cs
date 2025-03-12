28cd3b9760796468316298a0e77e0f2c66b07e49
rodolfo.bortoluzzi <rodolfo.bortoluzzi@bit.com.br>
2025-03-11 21:48:31 -0300

    view contatosPage

---
+++ MinhaAgenda/Views/ContatosPage.xaml.cs
@@ -0,0 +1,39 @@
+namespace MinhaAgenda.Views;
+
+public partial class ContatosPage : ContentPage
+{
+	public ContatosPage()
+	{
+		InitializeComponent();
+	}
+
+    private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void SearchBar_SearchButtonPressed(object sender, EventArgs e)
+    {
+
+    }
+
+    private void listaContatos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+
+    }
+
+    private void listaContatos_ItemTapped(object sender, ItemTappedEventArgs e)
+    {
+
+    }
+
+    private void Apagar_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnAdicionar_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
